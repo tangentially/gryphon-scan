FIX laser calibration pattern-to-camera angle calculation
Angle has to be calculated within camera XZ plane
not the global camera_Z_axis-to-pattern_Z_axis angle.

Fix laser calibration for non zero camera-to-turntable plane angle. Which
is actually all setups.

src/horus/engine/calibration/laser_triangulation.py
@@ -50,9 +50,8 @@ class LaserTriangulation(MovingCalibration):
         plane = self.image_detection.detect_pattern_plane(pose)
         if plane is not None:
             distance, normal, corners = plane
-
             # Angle between the pattern and the camera
-            alpha = np.rad2deg(math.acos(normal[2])) * math.copysign(1, normal[0])
+            alpha = np.rad2deg(math.acos(normal[2]/np.linalg.norm( (normal[0], normal[2]) ))) * math.copysign(1, normal[0])
             if abs(alpha) < 30:
                 self.image_capture.flush_laser(14)
                 for i in xrange(2):